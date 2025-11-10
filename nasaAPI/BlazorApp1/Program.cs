90bf109815d6ad0b5ee4f94bb9094e58d9875937
simo <sbrunelli01@gmail.com>
2025-11-10 13:34:14 +0100

    nasaAPI blazor

---
+++ nasaAPI/BlazorApp1/Program.cs
@@ -0,0 +1,34 @@
+using BlazorApp1.Components;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        // Register HttpClient properly
+        builder.Services.AddHttpClient();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
